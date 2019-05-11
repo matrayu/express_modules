c76738ebbab25180eb8e0e6cb91cbb2c59e3c9dc
matrayu <northridge614@gmail.com>
2019-05-11 07:46:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* npm run dev */
+
 const express = require('express');
 const morgan = require('morgan');
 
@@ -105,7 +107,7 @@ app.get('/lotto', (req, res) => {
         return res.status(400).send('Please enter atleast 6 numbers');
     }
 
-    const min = 1, max = 10, randomArr=[], foundArr=[];
+    const min = 1, max = 20, randomArr=[], foundArr=[];
     let message = '';
     while(randomArr.length < 6) {
         let randomNum = Math.floor(Math.random() * (+max - +min))
@@ -138,6 +140,98 @@ app.get('/lotto', (req, res) => {
 
     res.send(message);
 });
+
+app.get('/hello', (req, res) => {
+    res
+        .status(200)
+        .send('Hello!!!');
+});
+
+app.get('/error', (req, res) => {
+    res.status(500).send('There was an issue!');
+});
+
+app.get('/nothing', (req, res) => {
+    res
+        .status(204)
+        .end();
+});
+
+app.get('/video', (req, res) => {
+    const video = {
+        title: 'Cats falling over',
+        description: '15 minutes of great videos',
+        length: '15.40'
+    };
+    res.json(video)
+})
+
+app.get('/colors', (req, res) => {
+    const colors = [
+        {
+            name: "red",
+            rgb: "FF0000"
+        },
+        {
+            name: "blue",
+            rgb: "0000FF"
+        },
+        {
+            name: "green",
+            rgb: "00FF00"
+        },
+    ];
+    res.json(colors);
+})
+
+
+app.get('/grade', (req, res) => {
+    // get the mark from the query
+    const { mark } = req.query;
+  
+    // do some validation
+    if(!mark) {
+      // mark is required
+      return res
+        .status(400)
+        .send("Please provide a mark");
+    }
+  
+    const numericMark = parseFloat(mark);
+    if(Number.isNaN(numericMark)) {
+      // mark must be a number
+      return res
+        .status(400)
+        .send("Mark must be a numeric value");
+    }
+  
+    if(numericMark < 0 || numericMark > 100) {
+      // mark must be in range 0 to 100
+      return res
+        .status(400)
+        .send("Mark must be in range 0 to 100");
+    }
+  
+    if(numericMark >= 90) {
+      return res
+        .send("A");
+    } 
+  
+    if(numericMark => 80) {
+      return res
+        .send("B");
+    }
+  
+    if(numericMark >= 70) {
+      return res
+        .send("C");
+    }
+  
+    res
+      .send("F");
+  });
+
+
 
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000!')
